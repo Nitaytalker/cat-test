e6b99147286aed1d709d2546fac943e394da44d4
Nitaytalker <nitay68428@gmail.com>
2021-03-04 10:42:00 +0200

    server ----1

---
+++ cat_ReactApp/server/dbmodule.js
@@ -0,0 +1,64 @@
+const jsonFile = require('jsonFile')
+
+exports.getAll = async () => {
+    const fileData = await jsonFile.readFile('./data.json');
+    // console.log(fileData);
+    return fileData
+}
+
+exports.getById = async (id) => {
+    try {
+        const fileData = await jsonFile.readFile('./data.json');
+
+        let dataId = fileData.find(cat => cat.id == id);
+        if (!dataId) {
+            dataId = {}
+        }
+        //  console.log(dataId);
+        return dataId;
+    } catch (err) {
+        console.log(err);
+    }
+}
+
+exports.addUser = async (user) => {
+    try {
+        const fileData = await jsonFile.readFile('./data.json');
+        fileData.push(user);
+        const updateFile = await jsonFile.writeFile('./data.json', fileData)
+        return "success";
+    } catch (err) {
+        console.log(err);
+        return err
+    }
+}
+
+exports.deleteById = async (catToRemoveId) => {
+    try {
+        const oldData = await jsonFile.readFile('./data.json')
+        let remove = "error"
+        const newData = await oldData.filter((e) => {
+            if(e.id != catToRemoveId){
+                return true
+            }else{
+                remove = "success"
+                return false
+            }
+        })
+        const updateFile = await jsonFile.writeFile('./data.json', newData)
+        return (remove)
+    } catch (err) {
+        console.log(err);
+        return err
+    }
+}
+
+exports.getRandomCat = async ()=>{
+    try{
+        const cats = await jsonFile.readFile('./data.json')
+        return cats[Math.floor(Math.random() * cats.length)]
+    }catch(err){
+        return err
+    }
+    
+}
---
+++ cat_ReactApp/server/main.js
@@ -0,0 +1,46 @@
+const express = require("express")
+const app = express()
+const cors = require('cors');
+const dbModule = require('./dbmodule')
+
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(cors());
+
+app.get('/', (req, res) => {
+    dbModule.getAll().then(data => {
+        res.send(data);
+    })
+})
+
+app.get('/:id', (req, res) => {
+    // console.log(req.params);
+    dbModule.getById(req.params.id).then(data=>{
+        res.send(data)
+    })
+})
+
+app.get('/catapi/random',(req,res)=>{
+    dbModule.getRandomCat().then(data=>{
+        res.send(data)
+    })
+})
+app.post('/', (req, res) => {
+    // console.log(req.body);
+    dbModule.addUser(req.body).then(data => {
+        res.send({
+            "data": data
+        })
+    })
+})
+
+app.delete('/:id',(req,res)=>{
+    // console.log(req.params.id);
+    dbModule.deleteById(req.params.id).then(data=>{
+        res.send(data)
+    })
+})
+
+
+app.listen(3000,()=>{console.log("server run....");})
